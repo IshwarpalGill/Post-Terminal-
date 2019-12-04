b24dd5ddccad27d1175dafce048bd7512fe9fb4f
efhill2 <43968642+efhill2@users.noreply.github.com>
2019-12-03 20:38:02 -0500

    added methods to the payment class

--- GC_POSTerminalProject/Payment.cs
+++ GC_POSTerminalProject/Payment.cs
@@ -1,10 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace GC_POSTerminalProject
 {
     class Payment
     {
+        //Properties
+
+        public static double SubTotal { set; get; }
+        public static double SalesTax { set; get; }
+        public static double Total { set; get; }
+
+
+        public static double PayCash(double change)
+        {
+            //Get amount they are paying 
+            do
+            {
+                Console.WriteLine("Amount Tindered");
+
+                double amount = Convert.ToDouble(Console.ReadLine());
+
+                if (amount <= Total)
+                {
+                    change = amount - Total;
+                    Console.WriteLine($"{change}");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid amount");
+                }
+            } while (false);
+            return change;
+        }
+
+        public static bool CCPayment(bool valid)
+        {
+            string ccNumber;
+            string ccPattern = @"^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$";
+            string expDate;
+            string expDatePattern = @"^[0-9]{2}/[0-9]{2}$";
+            string cvv;
+            string cvvPattern = @"^[0-9]{3}$";
+
+            Regex ccRgx = new Regex(ccPattern);
+            Regex expRgx = new Regex(expDatePattern);
+            Regex cvvRgx = new Regex(cvvPattern);
+
+            do
+            {
+                Console.WriteLine("CC#");
+                ccNumber = Console.ReadLine();
+
+                if (ccRgx.IsMatch(ccNumber))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid CC#");
+                    valid = false;
+                }
+            } while (valid == false);
+
+
+            do
+            {
+                Console.WriteLine("ExpDate");
+                expDate = Console.ReadLine();
+
+                if (expRgx.IsMatch(expDate))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid expiration date");
+                    valid = false;
+                }
+            } while (valid == false);
+
+            do
+            {
+                Console.WriteLine("CVV");
+                cvv = Console.ReadLine();
+
+                if (ccRgx.IsMatch(cvv))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid CVV");
+                    valid = false;
+                }
+
+            } while (valid == false);
+            return (valid);
+        }
+        public static bool CheckPayment(bool cleared)
+        {
+            do
+            {
+                Console.WriteLine("Please enter the amount of check");
+                double amount = Convert.ToDouble(Console.ReadLine());
+
+                if (amount == Total)
+                {
+                    cleared = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please valid amount");
+                    cleared = false;
+                }
+            } while (cleared == false);
+
+            do
+            {
+                Console.WriteLine("Please enter check number");
+                string checkNum = Console.ReadLine();
+                cleared = true;
+
+            } while (cleared == false);
+            return (cleared);
+        }
+        public static double Tax(double subTotal)
+        {
+            double saleTax = subTotal * 0.06;
+            return (saleTax);
+        }
+
+        public static double STotal(int quantity, double itemPrice)
+        {
+            double subTotal = quantity * itemPrice;
+            return subTotal;
+        }
+
+
+        public static double GrandTotal(double subTotal, double saleTax)
+        {
+            double grandTotal = subTotal + saleTax;
+            return grandTotal;
+        }
     }
 }
